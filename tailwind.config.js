38c0e892cf0a27bcce856b3cba8d5b41f6ada9d2
Nabiya Yousaf <69292658+NabiyaYousaf@users.noreply.github.com>
2022-06-03 23:05:01 +0500

    Microsoft clone using tailwind css
    before running this project open your cmd and run npm i and after that npm run start

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'slider':"url('SS.webp')",
+        'slider2':"url('slider2.webp')",
+      }
+    },
+  },
+  plugins: [],
+}
